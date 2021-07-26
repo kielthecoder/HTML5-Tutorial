1da2c02f84ea60773df4e7ef80cbfeaf74a0050b
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2021-07-26 19:42:23 -0400

    it works! don't breathe on it!

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
-import CrComLib from '@crestron/ch5-crcomlib/build_bundles/umd/cr-com-lib';
-import WebXPanel from '@crestron/ch5-webxpanel';
+const webXpanel = require('../node_modules/@crestron/ch5-webxpanel/dist/cjs/index.js');
 
 const configuration = {
     host: '192.168.1.13',
-    ipId: '0x04',
+    ipId: '4'
 };
 
-WebXPanel.initialize(configuration);
+if (webXpanel.isActive) {
+    console.log(`WebXPanel version: ${webXpanel.getVersion()}`);
+    console.log(`WebXPanel build date: ${webXpanel.getBuildDate()}`);
 
+    webXpanel.default.initialize(configuration);
+}
+else {
+    console.log('Skipping WebXPanel since running on touchpanel');
+}
+
+const CrComLib = require('../node_modules/@crestron/ch5-crcomlib/build_bundles/cjs/cr-com-lib.js');
+window.CrComLib = CrComLib;
--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,6 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     entry: './src/app.js',
-    devServer: {
-        contentBase: './dist'
-    },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
