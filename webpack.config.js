2bbb95ed040ecf4b6e475742ddf195bc2b477bb8
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2021-07-28 08:11:52 -0400

    using webpack instead of copyfiles

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
 const path = require('path');
 
+const CopyPlugin = require('copy-webpack-plugin');
+
 module.exports = {
     mode: 'development',
     entry: './src/app.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: 'src/assets',
+                    to: 'assets/'
+                },
+                {
+                    from: 'src/*.(html|css)',
+                    to: '[name].[ext]'
+                }
+            ]
+        })
+    ]
 }
