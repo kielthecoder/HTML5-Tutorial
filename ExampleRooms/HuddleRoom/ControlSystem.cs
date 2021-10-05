80e44af5302c5b8254028fe047b68105216db833
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2021-10-05 16:17:51 -0400

    start of the c# code

--- ExampleRooms/HuddleRoom/ControlSystem.cs
+++ ExampleRooms/HuddleRoom/ControlSystem.cs
@@ -1,63 +1,42 @@
 using System;
-using Crestron.SimplSharp;                          	// For Basic SIMPL# Classes
-using Crestron.SimplSharpPro;                       	// For Basic SIMPL#Pro classes
-using Crestron.SimplSharpPro.CrestronThread;        	// For Threading
-using Crestron.SimplSharpPro.Diagnostics;		    	// For System Monitor Access
-using Crestron.SimplSharpPro.DeviceSupport;         	// For Generic Device Support
+using Crestron.SimplSharp;
+using Crestron.SimplSharpPro;
+using Crestron.SimplSharpPro.CrestronThread;
+using Crestron.SimplSharpPro.DeviceSupport;
+using Crestron.SimplSharpPro.UI;
 
 namespace HuddleRoom
 {
     public class ControlSystem : CrestronControlSystem
     {
-        /// <summary>
-        /// ControlSystem Constructor. Starting point for the SIMPL#Pro program.
-        /// Use the constructor to:
-        /// * Initialize the maximum number of threads (max = 400)
-        /// * Register devices
-        /// * Register event handlers
-        /// * Add Console Commands
-        /// 
-        /// Please be aware that the constructor needs to exit quickly; if it doesn't
-        /// exit in time, the SIMPL#Pro program will exit.
-        /// 
-        /// You cannot send / receive data in the constructor
-        /// </summary>
+        private BasicTriListWithSmartObject _tp;
+        private ushort _src;
+
         public ControlSystem()
             : base()
         {
             try
             {
                 Thread.MaxNumberOfUserThreads = 20;
-
-                //Subscribe to the controller events (System, Program, and Ethernet)
-                CrestronEnvironment.SystemEventHandler += new SystemEventHandler(_ControllerSystemEventHandler);
-                CrestronEnvironment.ProgramStatusEventHandler += new ProgramStatusEventHandler(_ControllerProgramEventHandler);
-                CrestronEnvironment.EthernetEventHandler += new EthernetEventHandler(_ControllerEthernetEventHandler);
             }
             catch (Exception e)
             {
-                ErrorLog.Error("Error in the constructor: {0}", e.Message);
+                ErrorLog.Error("Error in ControlSystem: {0}", e.Message);
             }
         }
 
-        /// <summary>
-        /// InitializeSystem - this method gets called after the constructor 
-        /// has finished. 
-        /// 
-        /// Use InitializeSystem to:
-        /// * Start threads
-        /// * Configure ports, such as serial and verisports
-        /// * Start and initialize socket connections
-        /// Send initial device configurations
-        /// 
-        /// Please be aware that InitializeSystem needs to exit quickly also; 
-        /// if it doesn't exit in time, the SIMPL#Pro program will exit.
-        /// </summary>
         public override void InitializeSystem()
         {
             try
             {
+                _tp = new Tsw1060(0x03, this);
+                _tp.OnlineStatusChange += tp_OnlineStatusChange;
+                _tp.SigChange += tp_SigChange;
 
+                if (_tp.Register() != eDeviceRegistrationUnRegistrationResponse.Success)
+                {
+                    ErrorLog.Error("Unable to register TSW-1060: {0}", _tp.RegistrationFailureReason);
+                }
             }
             catch (Exception e)
             {
@@ -65,85 +44,32 @@ namespace HuddleRoom
             }
         }
 
-        /// <summary>
-        /// Event Handler for Ethernet events: Link Up and Link Down. 
-        /// Use these events to close / re-open sockets, etc. 
-        /// </summary>
-        /// <param name="ethernetEventArgs">This parameter holds the values 
-        /// such as whether it's a Link Up or Link Down event. It will also indicate 
-        /// wich Ethernet adapter this event belongs to.
-        /// </param>
-        void _ControllerEthernetEventHandler(EthernetEventArgs ethernetEventArgs)
+        private void tp_OnlineStatusChange(GenericBase dev, OnlineOfflineEventArgs args)
         {
-            switch (ethernetEventArgs.EthernetEventType)
-            {//Determine the event type Link Up or Link Down
-                case (eEthernetEventType.LinkDown):
-                    //Next need to determine which adapter the event is for. 
-                    //LAN is the adapter is the port connected to external networks.
-                    if (ethernetEventArgs.EthernetAdapter == EthernetAdapterType.EthernetLANAdapter)
-                    {
-                        //
-                    }
-                    break;
-                case (eEthernetEventType.LinkUp):
-                    if (ethernetEventArgs.EthernetAdapter == EthernetAdapterType.EthernetLANAdapter)
-                    {
+            if (args.DeviceOnLine)
+            {
+                var tp = (BasicTriList)dev;
 
-                    }
-                    break;
+                tp.StringInput[1].StringValue = "Huddle Room";
+                tp.StringInput[2].StringValue = "x1234";
+
+                tp.UShortInput[1].UShortValue = _src;
             }
         }
 
-        /// <summary>
-        /// Event Handler for Programmatic events: Stop, Pause, Resume.
-        /// Use this event to clean up when a program is stopping, pausing, and resuming.
-        /// This event only applies to this SIMPL#Pro program, it doesn't receive events
-        /// for other programs stopping
-        /// </summary>
-        /// <param name="programStatusEventType"></param>
-        void _ControllerProgramEventHandler(eProgramStatusEventType programStatusEventType)
+        private void tp_SigChange(BasicTriList dev, SigEventArgs args)
         {
-            switch (programStatusEventType)
+            switch (args.Sig.Type)
             {
-                case (eProgramStatusEventType.Paused):
-                    //The program has been paused.  Pause all user threads/timers as needed.
-                    break;
-                case (eProgramStatusEventType.Resumed):
-                    //The program has been resumed. Resume all the user threads/timers as needed.
-                    break;
-                case (eProgramStatusEventType.Stopping):
-                    //The program has been stopped.
-                    //Close all threads. 
-                    //Shutdown all Client/Servers in the system.
-                    //General cleanup.
-                    //Unsubscribe to all System Monitor events
+                case eSigType.UShort:
+                    switch (args.Sig.Number)
+                    {
+                        case 1:
+                            _src = args.Sig.UShortValue;
+                            break;
+                    }
                     break;
             }
-
-        }
-
-        /// <summary>
-        /// Event Handler for system events, Disk Inserted/Ejected, and Reboot
-        /// Use this event to clean up when someone types in reboot, or when your SD /USB
-        /// removable media is ejected / re-inserted.
-        /// </summary>
-        /// <param name="systemEventType"></param>
-        void _ControllerSystemEventHandler(eSystemEventType systemEventType)
-        {
-            switch (systemEventType)
-            {
-                case (eSystemEventType.DiskInserted):
-                    //Removable media was detected on the system
-                    break;
-                case (eSystemEventType.DiskRemoved):
-                    //Removable media was detached from the system
-                    break;
-                case (eSystemEventType.Rebooting):
-                    //The system is rebooting. 
-                    //Very limited time to preform clean up and save any settings to disk.
-                    break;
-            }
-
         }
     }
 }
